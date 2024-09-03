8522d1fe9dfedf813db29184f688432186efd67d
graysonLL <liam.m.a.jones@gmail.com>
2024-09-03 22:10:14 +0800

    Add personalized greeting feature

--- hello.js
+++ hello.js
@@ -1,1 +1,13 @@
 console.log("hello world!");
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("What's your name? ", function (name) {
+  console.log(`Hello, ${name}!`);
+  rl.close();
+});
